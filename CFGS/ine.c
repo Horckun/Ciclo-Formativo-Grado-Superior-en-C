f588116aeaa87b124227da8990fbb10765f8386c
Horckun <horckun@gmail.com>
2018-09-26 15:16:06 +0200

    26/09/18
    ine

--- CFGS/ine.c
+++ CFGS/ine.c
@@ -10,8 +10,8 @@ void ine() {
 	
 	p("Una de las interacciones fundamentales descritas por la f%csica es la electricidad. Aunque conocidos desde antiguo, los fen%cmenos el%cctricos no empezaron a ser explicados de forma sistem%ctica hasta las postrimer%cas del siglo XVIII, y s%clo a mediados del XIX se descubri%c su estrecha relaci%cn con otra manifestaci%cn com%cn de la naturaleza: el magnetismo.\n\n",161,162,130,160,161,162,162,162,162,163);
 	
-	p("La electricidad en la naturaleza:\n");
-	p("---------------------------------\n\n");
+	p("- La electricidad en la naturaleza:\n");
+	p("-----------------------------------\n\n");
 	
 	p("La carga el%cctrica es una de las propiedades b%csicas de la materia. Aunque la comprensi%cn extensa de sus manifestaciones se resisti%c durante siglos al escrutinio de la ciencia, ya hacia el a%co 600 a. C. los fil%csofos griegos describieron con detalle el experimento por el cual una barra de %cmbar frotado atrae peque%cos pedacitos de paja u otro material ligero (electrizaci%cn por frotamiento).\n\n",130,160,162,162,164,162,160,164,162);
 	
@@ -19,14 +19,22 @@ void ine() {
 	
 	p("Por su naturaleza el%cctrica, los cuerpos f%csicos se clasifican en conductores, que transmiten la electricidad f%ccilmente, y aislantes o diel%cctricos, que oponen una resistencia elevada a su paso. Los semiconductores presentan una conductividad intermedia entre estas dos clases.\n\n",130,161,160,130);
 	
+	system("pause");
+	p("\n");
+	
+	p("- Cargas el%cctricas:\n",130);
+	p("--------------------\n\n");
+	
 	fprintf(archivo,"Interacción eléctrica. Ley de Coulomb. Estudio del campo eléctrico:\n");
 	fprintf(archivo,"===================================================================\n\n");
 	fprintf(archivo,"Una de las interacciones fundamentales descritas por la física es la electricidad. Aunque conocidos desde antiguo, los fenómenos eléctricos no empezaron a ser explicados de forma sistemática hasta las postrimerías del siglo XVIII, y sólo a mediados del XIX se descubrió su estrecha relación con otra manifestación común de la naturaleza: el magnetismo.\n\n");
-	fprintf(archivo,"La electricidad en la naturaleza:\n");
-	fprintf(archivo,"---------------------------------\n\n");
+	fprintf(archivo,"- La electricidad en la naturaleza:\n");
+	fprintf(archivo,"-----------------------------------\n\n");
 	fprintf(archivo,"La carga eléctrica es una de las propiedades básicas de la materia. Aunque la comprensión extensa de sus manifestaciones se resistió durante siglos al escrutinio de la ciencia, ya hacia el año 600 a. C. los filósofos griegos describieron con detalle el experimento por el cual una barra de ámbar frotado atrae pequeños pedacitos de paja u otro material ligero (electrización por frotamiento).\n\n");
 	fprintf(archivo,"Los fenómenos eléctricos, indivisiblemente unidos a los magnéticos, están presentes en todas partes, ya sea en las tormentas, la radiación solar o el cerebro humano. Modernamente, sus propiedades se aprovechan en múltiples campos de actividad, y la electricidad se ha convertido en una forma esencial de consumo y transporte de energía.\n\n");
 	fprintf(archivo,"Por su naturaleza eléctrica, los cuerpos físicos se clasifican en conductores, que transmiten la electricidad fácilmente, y aislantes o dieléctricos, que oponen una resistencia elevada a su paso. Los semiconductores presentan una conductividad intermedia entre estas dos clases.\n\n");
+	fprintf(archivo,"- Cargas eléctricas:\n");
+	fprintf(archivo,"--------------------\n\n");
 	
 	fclose(archivo);
 }
