97b458ae06da6e072b000907c5c5912edacb89db
Horckun <horckun@gmail.com>
2018-10-10 16:06:28 +0200

    10/10/18
    vein

--- CFGS/vein.c
+++ CFGS/vein.c
@@ -8,8 +8,13 @@ void vein() {
 	p("Vector Intensidad de campo el%cctrico. Potencial el%cctrico. Diferencia de potencial entre dos puntos de un campo el%cctrico.\n",130,130,130);
 	p("==========================================================================================================================\n\n");
 	
-	fprintf(archivo,"Vector Intensidad de campo eléctrico. Potencial eléctrico. Diferencia de potencial entre dos puntos de un campo eléctrico.\n");
+	p("- Potencial el%cctrico:\n",130);
+	p("----------------------\n\n");
+	
+	fprintf(archivo,"Vector Intensidad de campo eléctrico. Potencial eléctrico. Diferencia de potencial entre dos puntos de un campo eléctrico:\n");
 	fprintf(archivo,"==========================================================================================================================\n\n");
+	fprintf(archivo,"- Potencial eléctrico:\n");
+	fprintf(archivo,"----------------------\n\n");
 	
 	fclose(archivo);
 }
