ef894b5bc551c4d73c117ef77aacfadb118f6bc2
Horckun <horckun@gmail.com>
2018-10-12 15:26:30 +0200

    12/10/18
    vein

--- CFGS/vein.c
+++ CFGS/vein.c
@@ -13,11 +13,14 @@ void vein() {
 	
 	p("El potencial el%cctrico en un punto del espacio es una magnitud escalar que nos permite obtener una medida del campo el%cctrico en dicho punto a trav%cs de la energ%ca potencial electrost%ctica que adquirir%ca una carga si la situ%csemos en ese punto.\n\n",130,130,130,161,160,161,160);
 	
+	p("El hecho de que todas las magnitudes sean escalares, permite que el estudio del campo el%cctrico sea m%cs sencillo.\n\n",130,160);
+	
 	fprintf(archivo,"Vector Intensidad de campo eléctrico. Potencial eléctrico. Diferencia de potencial entre dos puntos de un campo eléctrico:\n");
 	fprintf(archivo,"==========================================================================================================================\n\n");
 	fprintf(archivo,"- Potencial eléctrico:\n");
 	fprintf(archivo,"----------------------\n\n");
 	fprintf(archivo,"El potencial eléctrico en un punto del espacio es una magnitud escalar que nos permite obtener una medida del campo eléctrico en dicho punto a través de la energía potencial electrostática que adquiriría una carga si la situásemos en ese punto.\n\n");
+	fprintf(archivo,"El hecho de que todas las magnitudes sean escalares, permite que el estudio del campo eléctrico sea más sencillo.\n\n");
 	
 	fclose(archivo);
 }
